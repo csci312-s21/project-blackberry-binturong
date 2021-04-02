ea155947ca0edfa071781bf4fc11d785750a86fb
Replit user <replituser@example.com>
2021-04-02 00:08:57 +0000

    sprint 0

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import  fetchMock  from "fetch-mock-jest";
+import { act } from "react-dom/test-utils";
+import Home from "../pages/index";
+
+describe("Top level integration tests", () => {
+  test("Smoke test", async () => {
+    render(<Home />);
+    await act(async () => {
+      await fetchMock.flush(true);
+    });
+  });
+});
