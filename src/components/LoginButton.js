480a73812f5e83d7f2231bf03b3fdad3c9d327ad
Replit user <replituser@example.com>
2021-04-16 01:19:50 +0000

    float button to the right

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -3,7 +3,7 @@ import styles from "../styles/LoginButton.module.css";
 
 export default function LoginButton({ loggedIn, handleClick }){
   return (
-    <div>
+    <div class={styles.loginContainer}>
       {(loggedIn)
         ? <input class={styles.loginButton} type="button" value="Out" onClick = {() => handleClick(false)}/>
         : <input class={styles.loginButton} type="button" value="In" onClick = {() => handleClick(true)}/>
