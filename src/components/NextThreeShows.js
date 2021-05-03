ed34c46696609463eae7197b3e611c7af0ae085f
Replit user <replituser@example.com>
2021-05-03 04:25:14 +0000

    Updated conditional rendering, moved filtering and moment function to index

--- src/components/NextThreeShows.js
+++ src/components/NextThreeShows.js
@@ -6,20 +6,12 @@
 */
 import PropTypes from "prop-types";
 import { showType } from "../lib/types.js";
-import { dayToInt, compareTwoShows } from "../lib/component-utils.js";
-import moment from "moment";
-import ShowSnippet from "./ShowSnippet.js";
 import styles from "../styles/NextThreeShows.module.css";
+import ShowSnippet from "../components/ShowSnippet.js"
 
 export default function NextThreeShows({ shows, handleClick }){
-  const now = moment();
 
-  const upcomingShows = shows.filter(
-    (show) => (dayToInt[show.time.day] === now.day()) && (show.time.hour > (now.hour() * 100)));
-
-  upcomingShows.sort((a, b) => compareTwoShows(a, b));
-
-  const nextThree = upcomingShows.slice(0, 3).map((show) => 
+  const nextThree = shows.map((show) => 
     <li className={styles.showListItem} key={show.id} onClick={() => handleClick(show)} data-testid="show snippet">
       <ShowSnippet show={show}/>
     </li>
@@ -29,7 +21,7 @@ export default function NextThreeShows({ shows, handleClick }){
     <div className={styles.nextThreeShows}>
       <div className={styles.header}>Today&apos;s next three shows:</div>
       <ul className={styles.showList}>{nextThree}</ul>
-      {(upcomingShows.length <= 3) && <div className={styles.message}>That&apos;s all for today!</div>}
+      {(nextThree.length < 3) && <div className={styles.message}>That&apos;s all for today!</div>}
     </div>
   );
 }
