d4012a7126600ce656823ef6d4c1bc3bed111729
Replit user <replituser@example.com>
2021-05-03 04:23:11 +0000

    Updated conditional rendering, moved filtering and moment function to index, updated proptypes, updated styling, added showdetails

--- src/components/DisplayCurrentShow.js
+++ src/components/DisplayCurrentShow.js
@@ -2,65 +2,29 @@
 
   This Component will display the current show title and DJs.
 
-  props - shows, an array of shows (Is this leaky abstraction)
+  props - shows, an array of shows
 */
 
-import PropTypes from "prop-types";
-import { dayToInt } from "../lib/component-utils.js";
-import moment from "moment";
-import ShowSnippet from "./ShowSnippet.js";
+import { showType } from "../lib/types.js";
 import styles from "../styles/DisplayCurrentShow.module.css";
 
 
-export default function DisplayCurrentShow({ shows }){
-  const now = moment();
+export default function DisplayCurrentShow({ show, handleClick }){
 
-  const upcomingShows = shows.filter(
-    (show) => (dayToInt[show.time.day] === now.day()) && (show.time.hour === (now.hour() * 100)));
-
-  let showExists = (typeof upcomingShows[0] !== 'undefined')
+  const showExists = (typeof show !== "undefined")
 
   return (
     <div className={styles.nextThreeShows}>
       <div className={styles.header}>ON AIR</div>
-      {!showExists && <div className={styles.show}> No current show :(( </div>}
-      {showExists && <div className={styles.show}>Current Show: <strong>{upcomingShows[0].title}</strong></div>}
-      {showExists && <div className={styles.show}><em>DJs: {upcomingShows[0].DJs.join(', ')}</em></div>}
-      {showExists && <div className={styles.show}> Call the DJ: 802 443 6423 </div>}
-      {(upcomingShows.length <= 3) && <div className={styles.message}>TUNE IN!</div>}
+      {!showExists && <div className={styles.infoText}> No current show :(( </div>}
+      {showExists && <div onClick={() => handleClick(show)} className={styles.show}>Current Show: <strong>{show.title}</strong></div>}
+      {showExists && <div className={styles.infoText}><em>DJs: {show.DJs.join(", ")}</em></div>}
+      {showExists && <div className={styles.infoText}> Call the DJ: 802 443 6423 </div>}
+      <div className={styles.message}>TUNE IN!</div>
     </div>
   );
 }
 
 DisplayCurrentShow.propTypes = {
-  shows: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      DJs: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-      description: PropTypes.string.isRequired,
-      time: PropTypes.shape({
-        day: PropTypes.string.isRequired,
-        hour: PropTypes.number.isRequired,
-        duration: PropTypes.number.isRequired,
-      }).isRequired,
-      genres: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-      id: PropTypes.number.isRequired,
-    }).isRequired
-  ).isRequired
+  show: showType.isRequired
 };
-
-
-// export default function DisplayCurrentShow({ shows}){
-//   const now = moment();
-
-//   const currentShow = shows.filter((show) => (dayToInt[show.time.day] === now.day()) && (show.time.hour === (now.hour() * 100)));
-
-//   //let displayed = currentShow[0].title;
-//   console.log(currentShow);
-  
-//   return (
-//     <div>
-      
-//     </div>
-//   );
-// }
