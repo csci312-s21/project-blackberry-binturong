ece722ca6a2759d220405a9eacb51c0efc20fa02
Replit user <replituser@example.com>
2021-05-03 04:31:34 +0000

    Imported in moment, styling, and global functions necessary to determine the currentShow and nextThree shows allowing us to avoid leaky abstraction by passing all the shows to the individual components. Required changes in rendering logic and added the isOnAir boolean variable.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,18 @@ import ShowOTW from "../components/ShowOTW.js";
 import DisplayCurrentShow from "../components/DisplayCurrentShow";
 import ShowDetails from "../components/ShowDetails.js";
 import Head from "next/head";
+
+import moment from "moment";
 import shows from "../../data/shows.json";
 import styles from "../styles/Home.module.css";
 
 import { useState } from "react";
+import { dayToInt, compareTwoShows } from "../lib/component-utils.js";
 
 export default function WRMCWebsite() {
   const [allShows] = useState(shows);
   const [loggedIn, setLoggedIn] = useState(false);
-  const [sotw] = useState(allShows[5]); //placeholder, eventually we will want a callback: "setSotw"
+  const [sotw] = useState(allShows[6]); //placeholder, eventually we will want a callback: "setSotw"
   const [page, setCurrentPage] = useState("Home");
   const [selectedShow, setSelectedShow] = useState();  // state for displaying ShowDetails
   const pageList = ["Home", "Blog", "Schedule", "Community", "About"];
@@ -31,11 +34,24 @@ export default function WRMCWebsite() {
     setCurrentPage("Show Details");
   }
 
+  // determines the current and next three shows
+  const now = moment();
+
+  const upcomingShows = shows.filter(
+    (show) => (dayToInt[show.time.day] === now.day()) && (show.time.hour >= (now.hour() * 100)));
+
+  upcomingShows.sort((a, b) => compareTwoShows(a, b));
+
+  const isOnAir = upcomingShows[0].time.hour === now.hour() * 100;
+
   const placeholderPages = {
     // TODO: we should create a container component for the Home page
     // (and any other pages that end up having multiple components)
     "Home" : <div>
-              <ShowOTW show={sotw} handleClick={clickShow}/> <p>{""}</p> <DisplayCurrentShow shows = {allShows}/> <p>{""}</p> <NextThreeShows shows={allShows} handleClick={clickShow}/>
+              <ShowOTW show={sotw} handleClick={clickShow}/> <p>{""}</p>
+              {isOnAir ? <DisplayCurrentShow show = {upcomingShows[0]} handleClick={clickShow}/> : <DisplayCurrentShow show = {shows.find(show => show.id === 12345)} handleClick={clickShow}/>}
+              <p>{""}</p>
+              {isOnAir ? <NextThreeShows shows={upcomingShows.slice(1,4)} handleClick={clickShow}/> : <NextThreeShows shows={upcomingShows.slice(0,3)} handleClick={clickShow}/>} 
              </div>,
     "Blog" : <h2>This is the blog</h2>,
     "Schedule" : <h2>This is the schedule</h2>,
