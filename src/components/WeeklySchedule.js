f18fab8a9af18bd8b5e55aff6cad9d8eb0504979
Sam Chester <>
2021-05-04 00:21:28 +0000

    updates

--- src/components/WeeklySchedule.js
+++ src/components/WeeklySchedule.js
@@ -1,43 +1,65 @@
 import PropTypes from "prop-types";
 import WeeklyShow from "./WeeklyShow";
 import { getDayInt } from "../lib/globals.js";
+import styles from "../styles/WeeklyShow.module.css";
 
 export default function WeeklySchedule({ shows }){
   const showsArr = [];
+  const days = ['','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-  showsArr.push(
-    ['','M', 'T', 'W', 'Th', 'F', 'S', 'S']
-  );
+  const firstRow = days.map((d) => {
+    return (<div className={styles.scheduleDay}>
+              {d}
+            </div>)
+  })
+
+  showsArr.push(firstRow);
 
   // Add useEffect when writing the onHover function
   for (let i = 0; i < 24; i++){
     showsArr.push([]);
     for (let l = 0; l < 8; l++){
       if (l===0){
-        showsArr[showsArr.length-1].push(i+":00");
+        showsArr[showsArr.length-1].push(<div className={styles.scheduleTime}>{i+":00"}</div>);
       }
-      else {showsArr[showsArr.length-1].push(<WeeklyShow show = {{}}/>)};
+      //else {showsArr[showsArr.length-1].push(<WeeklyShow show = {{}}/>)};
+      else {showsArr[showsArr.length-1].push(undefined)};
     }
   }
   
   shows.forEach((s) => {
     let day = getDayInt(s.time.day)+1;
-    console.log(day)
     let time = (s.time.hour / 100)+1;
     let genre = s.genre;
     showsArr[time][day] = <WeeklyShow show = {s}/>
-  }) 
+  })
+
 
   const table = showsArr.map((item) => {
-    return (
-      <tr>
-        {item.map((i) => {
-          return (<td>
-                    {i}
-                  </td>);
-        })}
-      </tr>
-    );
+    let count = 0;
+    item.forEach((unit) => {
+      if (unit === undefined) {
+        count += 1;
+      }
+    });
+    
+    if (count < 7) {
+      return (
+        <tr>
+          {item.map((i) => {
+            let result = i;
+            if (i === undefined) {
+              result = <WeeklyShow show = {{}}/>
+            }
+
+            return (<td>
+                      {result}
+                    </td>);
+          })}
+        </tr>
+      );
+    }
+    
   });
 
   return (
--- src/components/WeeklyShow.js
+++ src/components/WeeklyShow.js
@@ -1,20 +1,73 @@
 import PropTypes from "prop-types";
 import styles from "../styles/WeeklyShow.module.css";
+import { red } from "color-name";
 
 export default function WeeklyShow({ show }){
+
   if (show === {} || show === undefined){
     return <div className={styles.WeeklyShow}>
     </div>
   }
-  let djs = show.DJs;
-  if (show.DJs !== undefined) {
-    djs = show.DJs.join(", ");
+
+  const colors = {
+    "blues": "#2994A3",
+    "eclectic": "#00B1DA",
+    "folk": "#14CD96",
+    "indie": "#0033CC",
+    "metal": "#D3D3D3",
+    "punk": "#190707",
+    "rock": "#F50046",
+    "classical": "#FF918B",
+    "electronic": "#00FF43",
+    "fruit": "#005DD0",
+    "jazz": "#6C29A3",
+    "news": "#9B9B9B",
+    "r&b / soul": "#D0A9F5",
+    "sports": "#2E3B0B",
+    "comedy": "#FFD700",
+    "exec": "#228B22",
+    "funk": "#FF9840",
+    "kpop": "#89CFF0",
+    "pop": "#FF00FF",
+    "radio theatre": "#00FFFF",
+    "talk": "#FA3F74",
+    "dream pop": "#E987F2",
+    "experimental": "#EB42F4",
+    "hip hop": "#FA7198",
+    "latin": "#2A52BE",
+    "pop punk": "#190707",
+    "reggae": "#0AC02B",
+    "world": "#FFB700"
   }
 
+  let djs = show.DJs;
+  if (show.DJs !== undefined) {
+    djs = show.DJs.join(", ")
+  }
+
+  let genrecolor;
+  if (show.genres !== undefined) {
+    const g = show.genres[0].toLowerCase();
+    genrecolor = colors[g];
+
+    if (genrecolor === undefined) {
+      genrecolor = "#A8A8A8";
+    }
+  }
+  
+  if (genrecolor === undefined) {
+    genrecolor = "#DDDDDD";
+  }
+  
   return (
-    <div className={styles.WeeklyShow}>
+    /*
+    <div className={styles.WeeklyShow} style={{"background-color": genrecolor}}>
       <p className={styles.ShowTitle}>{show.title}</p>
       <p>{djs}</p>
+    </div>
+    */
+    <div className={styles.WeeklyShow} style={{"background-color": genrecolor}}>
+      <span className={styles.ShowTitle}>{show.title}<br/>{djs}</span>
     </div>
   )
 };
