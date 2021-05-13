d5b6e9c12b6004159987346385e3755874f27535
Sam Chester <>
2021-05-13 21:47:31 +0000

    Wrapped list on day-filter view

--- src/components/DaySchedule.js
+++ src/components/DaySchedule.js
@@ -7,15 +7,58 @@ import { showType } from "../lib/types.js";
 export default function DaySchedule({ shows , day}){
   const showList = shows.filter((s) => getDayString(s.time.day) === day)
   const showListSorted = showList.sort(function(a, b){return a.time.hour - b.time.hour});
-  const showsDisplayed = showListSorted.map((show) => 
-    <li className={styles.showList} key={show.id} data-testid="day show">
-      <DayShow show={show}/>
-    </li>
-  );
 
+  //this is stupid code: but its the only solution I can come up with...
+ let showsDisplayedRow1 = []
+ let showsDisplayedRow2 = []
+ let showsDisplayedRow3 = []
+ let showsDisplayedRow4 = []
+  if (showListSorted.length > 7) {
+    showsDisplayedRow1 = showListSorted.map((show, index) => {
+      if (index < 7){
+      return <td className={styles.showList} key={show.id} data-testid="day  show">
+        <DayShow show={show}/>
+      </td>
+      }
+    })
+    showsDisplayedRow2 = showListSorted.map((show, index) => {
+      if (index >= 7){
+      return <td className={styles.showList} key={show.id} data-testid="day  show">
+        <DayShow show={show}/>
+      </td>
+      }
+    });
+    showsDisplayedRow3 = showListSorted.map((show, index) => {
+      if (index >= 14){
+      return <td className={styles.showList} key={show.id} data-testid="day  show">
+        <DayShow show={show}/>
+      </td>
+      }
+    });
+    showsDisplayedRow4 = showListSorted.map((show, index) => {
+      if (index >= 21){
+      return <td className={styles.showList} key={show.id} data-testid="day  show">
+        <DayShow show={show}/>
+      </td>
+      }
+    });
+  }
+
+  if (showListSorted.length <= 7) {
+    showsDisplayedRow1 = showListSorted.map((show) => 
+      <td className={styles.showList} key={show.id} data-testid="day show">
+        <DayShow show={show}/>
+      </td>
+    );
+  }
+
+  console.log(showListSorted)
   return (
     <div data-testid="day schedule">
-      <ul className={styles.inline} >{showsDisplayed}</ul>
+      <tr className={styles.inline} >{showsDisplayedRow1}</tr>
+      <tr className={styles.inline} >{showsDisplayedRow2}</tr>
+      <tr className={styles.inline} >{showsDisplayedRow3}</tr>
+      <tr className={styles.inline} >{showsDisplayedRow4}</tr>
     </div>
   );
 }
