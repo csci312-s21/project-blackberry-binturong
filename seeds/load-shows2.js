99b8612c0e3b8adda6cd9866308693a1b874066f
Sam Chester <>
2021-05-17 23:05:30 +0000

    Seed!

--- seeds/load-shows2.js
+++ seeds/load-shows2.js
@@ -11,9 +11,44 @@ exports.seed = function (knex, Promise) {
   knex("Playlists").del();
   knex.batchInsert("Playlists", playlists, 100);
   
+
+  let djMap = [];
+  let djRecord= [];
+  shows.forEach((f) => {
+    f.DJs.forEach((d) => { 
+      let id;
+      if (!djRecord.includes(d)){
+        id = djRecord.length;
+        djRecord.push(d);
+      }
+      else {
+        for(i=0; i<djRecord.length; i++){
+          if (d === djRecord[i]){
+            id = i;
+          }
+        }
+      }
+      djMap.push({'show_id': f.id, 'DJs_id': id})
+    })
+  });
+  console.log(djMap);
+  console.log(djRecord)
+  knex("ShowDJs").del();
+  knex.batchInsert("ShowDJs", djMap, 100);
+
   const contentsDJs = fs.readFileSync('./data/DJs.json');
   const djs = JSON.parse(contentsDJs);
+  djsUpdated = djs.map((d) =>{
+    let id;
+    for(i=0; i<djRecord.length; i++){
+          if (d.name === djRecord[i]){
+            id = i;
+          }
+        }
+    return {name: d.name, email:d.email, id:id}
+  })
+  console.log(djsUpdated)
   knex("DJs").del();
-  knex.batchInsert("DJs", djs, 100);
+  knex.batchInsert("DJs", djsUpdated, 100);
   
 };
--- src/lib/backend-utils.js
+++ src/lib/backend-utils.js
@@ -4,3 +4,27 @@ import knexInitializer from 'knex';
 const knex = knexInitializer(
   knexConfig[process.env.NODE_ENV || 'development']
 );
+
+export async function getShows() {
+  const rows = await knex("Show").select();
+return rows;
+}
+
+export async function getShow(id) {
+  const articles = await knex("Show").where({id:id}).select();
+  if (articles.length !== 1) {
+    return null;
+  }
+  else {
+    return articles[0]
+  }
+}
+
+export async function getDJs(id){
+  const djs = await knex.select("name")
+    .from("ShowDJs")
+    .join("DJs", "DJs.id", "ShowDJs.DJs_id")
+    .where({"show_id":id});
+  const genreArr = genre.map((g) => g.genre);
+  return genreArr;
+}
