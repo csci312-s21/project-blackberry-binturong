c98a5f5866daa193ac24779dbbf554fbc9a7c16a
Sam Chester <>
2021-05-17 22:09:05 +0000

    Makin tables?

--- migrations/20210509204250_load-shows2.js
+++ migrations/20210509204250_load-shows2.js
@@ -1,6 +1,6 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('Show', (table) => {
-    table.increments('id');
+    table.integer('id');
     table.string('title');
     table.string('DJs');
     table.text('description');
@@ -8,9 +8,33 @@ exports.up = function (knex, Promise) {
     table.string('day').notNullable();
     table.decimal('hour').notNullable(); 
     table.decimal('duration').notNullable();
+  })
+  .createTable('Playlist', (table) => {
+    table.integer('id');
+    table.string('date');
+    table.integer('showId').references("Show.id").onDelete("CASCADE");
+  })
+  .createTable('Song', (table) => {
+    table.integer('id');
+    table.string('time');
+    table.string('title');
+    table.string('album_art');
+    table.string('album');
+    table.integer('playlistId').references("Playlist.id").onDelete("CASCADE");
+  })
+  .createTable('DJs', (table) => {
+    table.string('name');
+    table.string('email');
+    table.integer('id');
+  })
+  .createTable('ShowDJs', (table) => {
+    table.integer("show_id");
+    table.integer("DJs_id");
+    table.foreign("show_id").references("Show.id");
+    table.foreign("DJs_id").references("DJs.id");
   });
 };
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists('Article');
+  return knex.schema.dropTableIfExists('Show').dropTableIfExists('Playlist').dropTableIfExists('Song').dropTableIfExists('DJs').dropTableIfExists('ShowDJs');
 };
--- seeds/load-shows2.js
+++ seeds/load-shows2.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 
 exports.seed = function (knex, Promise) {
-  const contents = fs.readFileSync('./data/shows.json');
-  const data = JSON.parse(contents);
+  const contentsShow = fs.readFileSync('./data/shows.json');
+  const shows = JSON.parse(contentsShow);
+  knex("Show").del();
+  knex.batchInsert("Show", shows, 100);
 
-  // Deletes ALL existing entries
-  // Use batch insert because we have too many articles for simple insert
-  return knex('Show')
-    .del()
-    .then(() => knex.batchInsert('Show', data, 100));
+  const contentsPlaylist = fs.readFileSync('./data/playlists.json');
+  const playlists = JSON.parse(contentsPlaylist);
+  knex("Playlists").del();
+  knex.batchInsert("Playlists", playlists, 100);
+  
+  const contentsDJs = fs.readFileSync('./data/DJs.json');
+  const djs = JSON.parse(contentsDJs);
+  knex("DJs").del();
+  knex.batchInsert("DJs", djs, 100);
 };
