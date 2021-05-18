bc949ac282c6f37bf9b112acb3724267c65dd1b3
Replit user <replituser@example.com>
2021-05-18 16:00:50 +0000

    make seed file

---
+++ seeds/load-djs.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+exports.seed = function(knex) {
+  const djContents = fs.readFileSync("./data/djs.json");
+  const data = JSON.parse(djContents);
+
+  return knex('DJs')
+    .del()
+    .then(() => knex.batchInsert('DJs', data, 100));
+};
