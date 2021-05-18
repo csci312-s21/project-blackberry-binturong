ea9286f38265a405b9264eaf25d1451014c92499
Replit user <replituser@example.com>
2021-05-18 15:45:42 +0000

    setup migration

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './wrmc.sqlite3',
+    },
+    useNullAsDefault: true,
+  },
+  test: {
+    client: 'sqlite3',
+    connection: ":memory:",
+    useNullAsDefault: true,
+    seeds: {
+      directory: './seeds/test',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    ssl: true,
+  },
+};
