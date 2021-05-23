521a781c2f3b32e4ac87a26155e2788689851529
Replit user <replituser@example.com>
2021-05-23 13:17:16 +0000

    SongInput tests

--- src/__tests__/SongInput.test.js
+++ src/__tests__/SongInput.test.js
@@ -3,7 +3,9 @@
 Tests for SongInput.js
 
 */
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import fetchMock from "fetch-mock-jest";
+import { root, key } from "../lib/api.js";
 import SongInput from "../components/SongInput";
 
 const sampleTitle = "Sample Title";
@@ -25,7 +27,7 @@ describe("SongInput tests", () => {
 
   beforeEach(() => {
     handler.mockReset();
-    const sampleSong = {title: "", artist: "", album: "", id: 0, playlistID: 0};
+    const sampleSong = {title: "", artist: "", album: "", albumArt:"https://wrmc.middlebury.edu/wp-content/themes/wrmc/images/music-med.png", id: 0, playlistID: 0};
     render(<SongInput complete={handler} song={sampleSong} savedInit={false}/>);
   });
 
@@ -88,19 +90,87 @@ describe("SongInput tests", () => {
 
   });
 
-  test("When a song is entered, the text on the enter button changes to update", () => {
-    
-    populateTextInputs();
+});
 
-    let enterButton = screen.getByRole("button", { name: "Enter" });
-    fireEvent.click(enterButton);
+describe("Album art tests", () => {
 
-    const updateButton = screen.getByRole("button", { name: "Update" });
-    expect(updateButton).toBeInTheDocument();
+  const song = {
+    artist: "Cher",
+    album: "Believe",
+    title: "Strong Enough"
+  };
 
-    enterButton = screen.queryByRole("button", { name: "Enter" });
-    expect(enterButton).not.toBeInTheDocument();
+  const goodResponse = {
+    album: {
+      name: "Believe",
+      artist: "Cher",
+      mbid: "63b3a8ca-26f2-4e2b-b867-647a6ec2bebd",
+      url: "https://www.last.fm/music/Cher/Believe",
+      image: [{ "#text": "https://lastfm.freetls.fastly.net/i/u/34s/3b54885952161aaea4ce2965b2db1638.png", size: "small" }]
+    }
+  };
 
+  const badResponse = { "error": 6, "message": "Album not found", "links": [] };
+
+  const handler = jest.fn();
+
+  beforeAll(() => {
+    fetchMock.reset();
   });
 
+  beforeEach(() => {
+    handler.mockReset();
+    const sampleSong = {title: "", artist: "", album: "", albumArt:"https://wrmc.middlebury.edu/wp-content/themes/wrmc/images/music-med.png", id: 0, playlistID: 0};
+    render(<SongInput complete={handler} song={sampleSong}/>);
+  });
+
+  test("Image appears when component is first rendered", () => {
+    let image = screen.getByRole("img");
+    expect(image).toBeInTheDocument();
+    
+    const titleInput = screen.getByRole("textbox", { name: "Title" });
+    expect(titleInput).toBeInTheDocument();
+  });
+
+  test("Correct image appears when a valid album is entered", async () => {
+    fetchMock.get(`${root}?method=album.getinfo&api_key=${key}&artist=${song.artist}&album=${song.album}&format=json`, () => goodResponse);
+
+    const titleInput = screen.getByRole("textbox", { name: "Title" });
+    const artistInput = screen.getByRole("textbox", { name: "Artist" });
+    const albumInput = screen.getByRole("textbox", { name: "Album" });
+
+    fireEvent.change(titleInput, { target: { value: song.title } });
+    fireEvent.change(artistInput, { target: { value: song.artist } });
+    fireEvent.change(albumInput, { target: { value: song.album } });
+
+    const enterButton = screen.getByRole("button", { name: "Enter" });
+    fireEvent.click(enterButton);
+
+    fetchMock.flush(true);
+    await waitFor(() => {
+      expect(handler).toBeCalled();
+    });
+    expect(screen.getByRole("img").src).toEqual("https://lastfm.freetls.fastly.net/i/u/34s/3b54885952161aaea4ce2965b2db1638.png");
+  });
+
+  test("Placeholder image appears when an invalid album is entered", async () => {
+    fetchMock.get("*", () => badResponse);
+
+    const titleInput = screen.getByRole("textbox", { name: "Title" });
+    const artistInput = screen.getByRole("textbox", { name: "Artist" });
+    const albumInput = screen.getByRole("textbox", { name: "Album" });
+
+    fireEvent.change(titleInput, { target: { value: "Bad title" } });
+    fireEvent.change(artistInput, { target: { value: "Bad artist" } });
+    fireEvent.change(albumInput, { target: { value: "bad album" } });
+
+    const enterButton = screen.getByRole("button", { name: "Enter" });
+    fireEvent.click(enterButton);
+
+    fetchMock.flush(true);
+    await waitFor(() => {
+      expect(handler).toBeCalled();
+    })
+    expect(screen.getByRole("img").src).toEqual("https://wrmc.middlebury.edu/wp-content/themes/wrmc/images/music-med.png");
+  });
 });
--- src/components/SongInput.js
+++ src/components/SongInput.js
@@ -7,12 +7,14 @@
 import PropTypes from "prop-types";
 import { songType } from "../lib/types.js";
 import { useState } from "react";
+import styles from "../styles/SongInput.module.css";
 import moment from "moment";
 
 export default function SongInput({ complete, song, savedInit }) {
   const [title, setTitle] = useState(song ? song.title : "");
   const [artist, setArtist] = useState(song ? song.artist : "");
   const [album, setAlbum] = useState(song ? song.album : "");
+  const [albumArt, setAlbumArt] = useState(song ? song.albumArt : "https://wrmc.middlebury.edu/wp-content/themes/wrmc/images/music-med.png");
   const [saved, setSaved] = useState(savedInit);
 
   const saveSong = (newSong) => {
@@ -22,38 +24,45 @@ export default function SongInput({ complete, song, savedInit }) {
   }
 
   return (
-    <div>
+    <div className={styles.row}> 
       <input 
         type="text" 
         placeholder="Enter song title"
         aria-label="Title"
         defaultValue={title}
+        className={styles.textinput}
         onChange={(event) => setTitle(event.target.value)}/>
       <input 
         type="text" 
         placeholder="Enter artist" 
         aria-label="Artist"
         defaultValue={artist}
+        className={styles.textinput}
         onChange={(event) => setArtist(event.target.value)}/>
       <input 
         type="text" 
         placeholder="Enter album"
         aria-label="Album"
         defaultValue={album}
+        className={styles.textinput}
         onChange={(event) => setAlbum(event.target.value)}/>
       <input 
         type="button"
-        value={saved ? "Update" : "Enter"}
+        className={styles.buttoninput}
+        value="Enter"
         disabled={(title === "") || (artist === "") || (album === "")}
         onClick={
-          () => saveSong({...song, title: title, artist: artist, album: album, timeAdded: moment().format("LT")})}/>
+          () => {
+            saveSong({...song, title: title, artist: artist, album: album, albumArt: albumArt, timeAdded: moment().toISOString()});
+          }}/>
       <input
         type="button"
+        className={styles.buttoninput}
         value="Delete"
         onClick={() => complete("delete", {...song})}/>
+      <img src={albumArt} className={styles.image}/>
     </div>
   );
-
 }
 
 SongInput.propTypes = {
