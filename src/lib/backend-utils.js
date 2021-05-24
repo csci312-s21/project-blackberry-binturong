1d6133673d7ab285933b0ed657a6d5961a56fae4
Replit user <replituser@example.com>
2021-05-24 21:18:22 +0000

    remove extra comma

--- src/lib/backend-utils.js
+++ src/lib/backend-utils.js
@@ -79,7 +79,7 @@ export async function getAllPlaylists() {
  * @returns the playlist with a new id attached
  */
 export async function addPlaylist(playlist) {
-  const [result] = await knex("Playlist").insert(playlist, , ["id"]);
+  const [result] = await knex("Playlist").insert(playlist, ["id"]);
   const playlistId = result.id ? result.id : result;
   return {...playlist, id: playlistId};
 }
