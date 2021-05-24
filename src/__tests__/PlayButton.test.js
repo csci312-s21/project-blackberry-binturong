cc5deee92b11c5c0b22eb73cce54a1e957b7a27a
Cliff <cliffyyc@outlook.com>
2021-05-23 20:00:27 -0400

    tests and removing files

--- src/__tests__/PlayButton.test.js
+++ src/__tests__/PlayButton.test.js
@@ -7,18 +7,15 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import PlayButton from "../components/PlayButton";
 
 describe("PlayButton tests", () => {
-  
   test("displays component", () => {
-    render(<PlayButton/>);
+    render(<PlayButton />);
     expect(screen.queryByText("LISTEN NOW")).toBeVisible();
   });
 
-   test("opens link to the boombox", () => {
-    render(<PlayButton/>);
+  test("opens link to the boombox", () => {
+    render(<PlayButton />);
     global.open = jest.fn();
-    fireEvent.click(screen.queryByRole("button", { name: "LISTEN NOW" }));
+    fireEvent.click(screen.queryByText("LISTEN NOW"));
     expect(global.open).toHaveBeenCalled();
-
-   })
-
+  });
 });
