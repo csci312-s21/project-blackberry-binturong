61003b54c269b35b0d6f4568dd98ee5b4d8179de
Replit user <replituser@example.com>
2021-05-24 18:27:21 +0000

    fix seed

--- seeds/load-shows-and-djs.js
+++ seeds/load-shows-and-djs.js
@@ -5,15 +5,6 @@ exports.seed = async function (knex) {
   const contentsShow = fs.readFileSync('./data/shows.json');
   const shows = JSON.parse(contentsShow);
 
-  const djMap = [];
-  shows.forEach((show) => {
-    show.DJs.forEach((id) => {
-      djMap.push({showId: show.id, djId: id});
-    });
-  });
-  await knex("ShowDJs").del();
-  await knex.batchInsert("ShowDJs", djMap, 100);
-
   const showData = shows.map(
     (show) => ({
       title: show.title, 
@@ -32,4 +23,13 @@ exports.seed = async function (knex) {
   const djData = JSON.parse(djContents);
   await knex("DJs").del();
   await knex.batchInsert("DJs", djData, 100);
+
+  const djMap = [];
+  shows.forEach((show) => {
+    show.DJs.forEach((id) => {
+      djMap.push({showId: show.id, djId: id});
+    });
+  });
+  await knex("ShowDJs").del();
+  await knex.batchInsert("ShowDJs", djMap, 100);
 };
