b57aea5ee8d64d15ca3b2ce2bdbfa1914e4599ea
Ethan Saxenian <ethansaxenian@gmail.com>
2021-06-07 15:55:17 -0400

    update data and migration table

--- migrations/20210509204250_initialize-tables.js
+++ migrations/20210509204250_initialize-tables.js
@@ -1,41 +1,42 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('Show', (table) => {
-    table.increments('id');
-    table.string('title').notNullable();
-    table.text('description');
-    table.string('genres').notNullable();
-    table.string('day').notNullable();
-    table.integer('hour').notNullable(); 
-    table.decimal('duration').notNullable();
-  })
-  .createTable('Playlist', (table) => {
-    table.increments('id');
-    table.string('date').notNullable();
-    table.integer('showId').references("Show.id").onDelete("CASCADE");
-    table.boolean('current').notNullable();
-  })
-  .createTable('Song', (table) => {
-    table.increments('id');
-    table.string('time');
-    table.string('title').notNullable();
-    table.string('artist').notNullable();
-    table.string('albumArt').notNullable();
-    table.string('album').notNullable();
-    table.integer('playlistId').references("Playlist.id").onDelete("CASCADE");
-  })
-  .createTable('DJs', (table) => {
-    table.increments('id');
-    table.string('name');
-    table.string('email').unique().notNullable();
-    table.boolean('exec').notNullable();
-  })
-  .createTable('ShowDJs', (table) => {
-    table.integer("showId");
-    table.integer("djId");
-    table.foreign("showId").references("Show.id");
-    table.foreign("djId").references("DJs.id");
-  })
-  .createTable("accounts", table => {
+  return knex.schema
+    .createTable("Show", (table) => {
+      table.increments("id");
+      table.string("title").notNullable();
+      table.text("description");
+      table.string("genres").notNullable();
+      table.string("day").notNullable();
+      table.integer("hour").notNullable();
+      table.decimal("duration").notNullable();
+    })
+    .createTable("Playlist", (table) => {
+      table.increments("id");
+      table.string("date").notNullable();
+      table.integer("showId").references("Show.id").onDelete("CASCADE");
+      table.boolean("current").notNullable();
+    })
+    .createTable("Song", (table) => {
+      table.increments("id");
+      table.string("time");
+      table.string("title").notNullable();
+      table.string("artist").notNullable();
+      table.string("albumArt");
+      table.string("album").notNullable();
+      table.integer("playlistId").references("Playlist.id").onDelete("CASCADE");
+    })
+    .createTable("DJs", (table) => {
+      table.increments("id");
+      table.string("name");
+      table.string("email").unique().notNullable();
+      table.boolean("exec").notNullable();
+    })
+    .createTable("ShowDJs", (table) => {
+      table.integer("showId");
+      table.integer("djId");
+      table.foreign("showId").references("Show.id");
+      table.foreign("djId").references("DJs.id");
+    })
+    .createTable("accounts", (table) => {
       table.increments("id").primary();
       table.string("compound_id", 255).notNullable();
       table.integer("user_id").notNullable();
@@ -53,7 +54,7 @@ exports.up = function (knex) {
       table.index("provider_id");
       table.index("user_id");
     })
-    .createTable("sessions", table => {
+    .createTable("sessions", (table) => {
       table.increments("id").primary();
       table.integer("user_id").notNullable();
       table.timestamp("expires").notNullable();
@@ -65,7 +66,7 @@ exports.up = function (knex) {
       table.unique("session_token");
       table.index("access_token");
     })
-    .createTable("users", table => {
+    .createTable("users", (table) => {
       table.increments("id").primary();
       table.string("name", 255);
       table.string("email", 255);
@@ -77,7 +78,7 @@ exports.up = function (knex) {
 
       table.unique("email");
     })
-    .createTable("verification_requests", table => {
+    .createTable("verification_requests", (table) => {
       table.increments("id").primary();
       table.string("identifier", 255).notNullable();
       table.string("token", 255).notNullable();
@@ -87,13 +88,13 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
   return knex.schema
-    .dropTableIfExists('Show')
-    .dropTableIfExists('Playlist')
-    .dropTableIfExists('Song')
-    .dropTableIfExists('DJs')
-    .dropTableIfExists('ShowDJs')
-    .dropTableIfExists('accounts')
-    .dropTableIfExists('sessions')
-    .dropTableIfExists('users')
-    .dropTableIfExists('verification_requests');
+    .dropTableIfExists("Show")
+    .dropTableIfExists("Playlist")
+    .dropTableIfExists("Song")
+    .dropTableIfExists("DJs")
+    .dropTableIfExists("ShowDJs")
+    .dropTableIfExists("accounts")
+    .dropTableIfExists("sessions")
+    .dropTableIfExists("users")
+    .dropTableIfExists("verification_requests");
 };
